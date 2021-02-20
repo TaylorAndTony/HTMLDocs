687623a38ccdd2ac3b0b466a0de94cede5b0f1a3
Taylor-PC <doyijian@163.com>
2021-02-20 21:00:50 +0800

    init

---
+++ SearchPage/main.js
@@ -0,0 +1,29 @@
+// return time like 20:34
+function getDate() {
+    date = new Date();
+    hour = date.getHours();
+    minute = date.getMinutes();
+    if (minute < 10) {
+        minute = '0' + minute;
+    }
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+    return `${hour}:${minute}`
+}
+// callback function to set the clock
+function setClock(){
+    document.getElementById("clock").innerHTML = getDate();
+}
+// animated function to show the clock
+function showClock(){
+    document.getElementById("clock").style.opacity = 1;
+}
+// main
+window.onload = function(){
+    setClock();
+    setInterval(setClock, 1000);
+    // setClock();
+    // 0.1s 后展示时钟，动画1s
+    setTimeout(showClock, 100);
+}
