02834deb72d55b4c416b169e015a76dc223aee11
TaylorAndTony <doyijian@163.com>
2022-04-27 09:26:14 +0800

    change style

--- FuckSportVideoPlayer/script.js
+++ FuckSportVideoPlayer/script.js
@@ -1,10 +1,22 @@
+let clicked = true;
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('video').volume = 0;
-    document.getElementById('video').loop = true;
-    document.getElementById('video').play();
+    let vid = document.getElementById('video');
+    vid.volume = 0;
+    vid.loop = true;
+    vid.play();
 
     document.getElementById('slider').oninput = function() {
         document.getElementById('video').playbackRate = this.value;
         document.getElementById('spd-value').innerText = this.value;
     }
+
+    vid.addEventListener('click', function(e) {
+        if (clicked) {
+            vid.style.width = '90%';
+        } else {
+            vid.style.width = '50%';
+        }
+        clicked = !clicked;
+    })
 })
